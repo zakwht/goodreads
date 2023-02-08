01c7906d640fdc62f4ba5102d01b100067681aeb
Zak White <zakwht@gmail.com>
2023-02-08 14:08:40 -0800

    vercel

--- api.js
+++ api.js
@@ -4,6 +4,7 @@ const { parse, getProgress } = require("./rss.js");
 
 const app = express();
 
+app.set('view engine', 'nunjucks.render')
 nunjucks.configure("", {
   autoescape: true,
   express: app
@@ -15,25 +16,27 @@ app.get("/goodreads/json", async (req, res) => {
   if (parsed.error) {
     return res.send(parsed.error.toString());
   }
-  res.send(parsed);
+  res.setHeader("Content-Type", "application/json")
+  res.json(parsed);
 });
 
 app.get("/goodreads", async (req, res) => {
   if (!req.query.user) return res.send("No user parameter specified.");
 
   if (req.query.user.replaceAll("o", 2).replaceAll("n", 3) == "j23s32w") {
-    res.render(__dirname + "/template.njk", {
+    res.setHeader("Content-Type", "image/svg+xml")
+    return res.render(__dirname + "/template.njk", {
       title: "The Winds of Winter",
       author: "George R. R. Martin",
       progress: "p. 524/3201",
       image: "https://64.media.tumblr.com/5c0f7e5cd95581b000498bc3789b2790/tumblr_mgday87lPU1r8fbwyo2_250.jpg",
       user: "User"
     });
-    return;
   }
 
   const parsed = await parse(req.query.user);
   if (parsed.error) return res.send(parsed.error);
+  res.setHeader("Content-Type", "image/svg+xml")
   res.render(__dirname + "/template.njk", {
     title: parsed.book.title,
     author: parsed.book.author,
